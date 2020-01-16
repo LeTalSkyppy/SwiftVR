773e6663c12814b16b9385c1cd8732476aa4fc75
Snowirbix <snowirbix@gmail.com>
2020-01-16 11:25:31 +0100

    Add Config

--- Assets/Swift/Scripts/Config.cs
+++ Assets/Swift/Scripts/Config.cs
@@ -3,6 +3,20 @@
 [System.Serializable]
 public class Config
 {
-    public Vector3 position;
-    public Quaternion rotation;
+    [System.Serializable]
+    public class Element
+    {
+        public string name;
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public Element (string name, Vector3 position, Quaternion rotation)
+        {
+            this.name = name;
+            this.position = position;
+            this.rotation = rotation;
+        }
+    }
+
+    public Element[] elements;
 }

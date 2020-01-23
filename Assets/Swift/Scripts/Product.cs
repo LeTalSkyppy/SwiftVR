ae2f9ec2b849140995ede7ee522f8f796c893ff0
LeTalSkyppy <alexandre.monnier@viacesi.fr>
2020-01-23 21:57:13 +0100

    test

---
+++ Assets/Swift/Scripts/Product.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Product : MonoBehaviour
+{
+    public GameObject target;
+    Vector3 diff = Vector3.zero;
+    Vector3 targetPosition = Vector3.zero;
+
+    Vector3 initialPosition = Vector3.zero;
+
+    float diffSpeed;
+
+    public const float speed = 30f;
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+    void Update()
+    {
+        if(target != null)
+        {
+            if(targetPosition == Vector3.zero)
+            {
+                targetPosition = target.transform.position;
+                diff = targetPosition - initialPosition;
+                diffSpeed = diff.magnitude;
+            }
+
+            if(diff == Vector3.zero)
+            {
+                diff = targetPosition - initialPosition;
+                diffSpeed = diff.magnitude;
+            }
+
+            if(targetPosition != target.transform.position)
+            {
+                targetPosition = target.transform.position;
+                diff = targetPosition - initialPosition;
+            }
+
+            Vector3 diffInstantT = targetPosition - transform.position;            
+            Vector3 diffCalcul = diff *(diffSpeed/diff.magnitude);
+            transform.position += diffCalcul/100f * speed * Time.deltaTime;
+
+            if(diffInstantT.magnitude < 0.2f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
